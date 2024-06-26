0d3d602d59e2e8b93eacea193e94aa5b93f20a2c
Suhani-1211 <129710778+Suhani-1211@users.noreply.github.com>
2024-06-26 20:51:14 +0530

    Update DegenToken.sol

--- contracts/DegenToken.sol
+++ contracts/DegenToken.sol
@@ -1,14 +1,36 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.18;
+pragma solidity ^0.8.20;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 
-contract DegenToken is ERC20, Ownable {
+contract DegenGamingToken is ERC20, Ownable {
+    constructor(address initialOwner) ERC20("DegenGamingToken", "DGT") Ownable(initialOwner){
+        transferOwnership(initialOwner); // Assuming the ownership of tokens should be transferred to the owner at contract creation. Adjust if that's not the case.
+    }
 
-    constructor() ERC20("Degen", "DGN") {}
+    function mint(address to, uint256 amount) public onlyOwner {
+         _mint(to, amount);
+    }
 
-        function mint(address to, uint256 amount) public onlyOwner {
-            _mint(to, amount);
+    function decimals() override public pure returns (uint8){
+        return 0;
+    }
+
+    function burn(uint256 amount) public {
+         _burn(msg.sender, amount);
+    }
+
+    function redeem(address from, uint256 amount) public onlyOwner {
+         _burn(from, amount);
+        // Additional logic for redeeming tokens for in-game items can be added here
+    }
+
+    function getBalance(address account) public view returns (uint256) {
+        return balanceOf(account);
+    }
+
+    function transferTokens(address to, uint256 amount) public returns (bool) {
+        return transfer(to, amount);
     }
 }
