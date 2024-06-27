754d34a0cd5f0e001f781a9a7e1b8ca2515fea8f
Suhani-1211 <129710778+Suhani-1211@users.noreply.github.com>
2024-06-27 17:14:09 +0530

    Update DegenToken.sol
    updation as peer project requirements

--- contracts/DegenToken.sol
+++ contracts/DegenToken.sol
@@ -1,3 +1,9 @@
+/*Minting new tokens: The platform should be able to create new tokens and distribute them to players as rewards. Only the owner can mint tokens.
+Transferring tokens: Players should be able to transfer their tokens to others.
+Redeeming tokens: Players should be able to redeem their tokens for items in the in-game store.
+Checking token balance: Players should be able to check their token balance at any time.
+Burning tokens: Anyone should be able to burn tokens, that they own, that are no longer needed.
+ */
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.20;
 
